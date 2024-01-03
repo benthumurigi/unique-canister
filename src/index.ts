56e86e45fb887989b129657f1f73aecc64f3bf28
benthumurigi <benthumurigi@gmail.com>
2024-01-03 17:04:35 +0000

    Refactored code to prevent code duplication by created a new function, findPollById

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ export function getAllPolls(): Vec<Poll> {
 // Define the update function to vote on a poll
 update
 export function vote(pollId: string, optionIndex: nat64): Result<string, string> {
-    const poll = pollStorage.find((poll) => poll.id === pollId);
+    const poll = findPollById(pollId);
     if (!poll) {
         return Result.Err(`Poll with ID ${pollId} not found`);
     }
@@ -65,10 +65,15 @@ export function vote(pollId: string, optionIndex: nat64): Result<string, string>
 // Define the query function to get the results of a poll
 query
 export function getPollResults(pollId: string): Result<Vec<nat64>, string> {
-    const poll = pollStorage.find((poll) => poll.id === pollId);
+    const poll = findPollById(pollId);
     if (!poll) {
         return Result.Err(`Poll with ID ${pollId} not found`);
     }
 
     return Result.Ok(poll.votes);
+}
+
+// Finds a specific poll by its id
+function findPollById(pollId: string): Opt<Poll> {
+    return pollStorage.find((poll) => poll.id === pollId);
 }
